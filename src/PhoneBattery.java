9bc4f5a5f897e02d3fa1daf8e86156a422806506
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-11-29 12:33:50 +0400

    PhoneBattery.java

--- src/PhoneBattery.java
+++ src/PhoneBattery.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+
 
 public class PhoneBattery {
+    public static void main(String[] args) {
+
+        int enerjiSeviyyesi = 100;//başlanğıc enerji səviyyəsi
+
+        //while dövrü - enerji>0 olduğu müddətcə
+        while(enerjiSeviyyesi>0){
+            //cari enerji səviyyəsini çap edirik
+            System.out.println("Cari enerji səviyyəsi: " + enerjiSeviyyesi + "%");
+            enerjiSeviyyesi -= 10;//enerji səviyyəsini 10 vahid azaldırıq
+        }
+
+        //enerji bitdikdə mesaj
+        System.out.println("Telefon söndü. Zəhmət olmasa enerji yığın.");
+    }
 }
